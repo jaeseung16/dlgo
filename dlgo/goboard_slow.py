cc26a51426ee142cc7d82efb5ca0a04c5c597161
Jae-Seung Lee <jaeseung@gmail.com>
2025-01-06 19:14:31 -0500

    feat: Add GoString

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -23,3 +23,30 @@ class Move():
         return Move(is_resign=True)
 
 
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = liberties
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(self.color, combined_stones, (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.stones == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+    

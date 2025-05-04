6350766dcfb37a0e352da0ff50dacd2a0eb8ff52
Jae-Seung Lee <jaeseung@gmail.com>
2025-05-04 10:48:32 -0400

    feat: Add networks

---
+++ dlgo/networks/fullyconnected.py
@@ -0,0 +1,13 @@
+from keras.layer import Dense, Activation, Flatten
+
+
+def layers(input_shape):
+    return [
+        Dense(128, input_shape=input_shape),
+        Activation('relu'),
+        Dense(128),
+        Activation('relu'),
+        Flatten(),
+        Dense(128),
+        Activation('relu'),
+    ]

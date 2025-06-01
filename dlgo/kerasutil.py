ed988529439a268f9d5b38ad20a432c465a5dc99
Jae-Seung Lee <jaeseung@gmail.com>
2025-06-01 05:55:41 -0400

    fix: Add h5 extension

--- dlgo/kerasutil.py
+++ dlgo/kerasutil.py
@@ -12,8 +12,8 @@ def save_model_to_hdf5_group(model, f):
     tempfd, tempfname = tempfile.mkstemp(prefix='tmp-kerasmodel')
     try:
         os.close(tempfd)
-        save_model(model, tempfname)
-        serialized_model = h5py.File(tempfname, 'r')
+        save_model(model, tempfname + ".h5")
+        serialized_model = h5py.File(tempfname + ".h5", 'r')
         root_item = serialized_model.get('/')
         serialized_model.copy(root_item, f, 'kerasmodel')
         serialized_model.close()
@@ -27,14 +27,14 @@ def load_model_from_hdf5_group(f, custom_objects=None):
     tempfd, tempfname = tempfile.mkstemp(prefix='tmp-kerasmodel')
     try:
         os.close(tempfd)
-        serialized_model = h5py.File(tempfname, 'w')
+        serialized_model = h5py.File(tempfname + ".h5", 'w')
         root_item = f.get('kerasmodel')
         for attr_name, attr_value in root_item.attrs.items():
             serialized_model.attrs[attr_name] = attr_value
         for k in root_item.keys():
             f.copy(root_item.get(k), serialized_model, k)
         serialized_model.close()
-        return load_model(tempfname, custom_objects=custom_objects)
+        return load_model(tempfname + ".h5", custom_objects=custom_objects)
     finally:
         os.unlink(tempfname)
 

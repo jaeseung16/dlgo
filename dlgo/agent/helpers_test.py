46d854c9333f9b02279951df32dc64b7b2babe60
Jae-Seung Lee <jaeseung@gmail.com>
2025-06-24 18:32:57 -0400

    fix: Correct the indentation

--- dlgo/agent/helpers_test.py
+++ dlgo/agent/helpers_test.py
@@ -8,18 +8,18 @@ from dlgo.gotypes import Player, Point
 class EyeTest(unittest.TestCase):
     def test_corner(self):
         board = Board(19, 19)
-        board.place_stone(Player.black, Point(1,2))
-        board.place_stone(Player.black, Point(2,2))
-        board.place_stone(Player.black, Point(2,1))
-        self.assertTrue(is_point_an_eye(board, Point(1,1), Player.black))
-        self.assertFalse(is_point_an_eye(board, Point(1,1), Player.white))
+        board.place_stone(Player.black, Point(1, 2))
+        board.place_stone(Player.black, Point(2, 2))
+        board.place_stone(Player.black, Point(2, 1))
+        self.assertTrue(is_point_an_eye(board, Point(1, 1), Player.black))
+        self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.white))
 
     def test_corner_false_eye(self):
         board = Board(19, 19)
         board.place_stone(Player.black, Point(1, 2))
         board.place_stone(Player.black, Point(2, 1))
         self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.black))
-        board.place_stone(Player.white, Point(2, 1))
+        board.place_stone(Player.white, Point(2, 2))
         self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.black))
 
     def test_middle(self):

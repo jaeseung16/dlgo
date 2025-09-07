9b1f178d94769b83ffb51ec63ea17a443e47d5e7
Jae-Seung Lee <jaeseung@gmail.com>
2025-09-07 10:59:00 -0400

    fix: Fix typos

--- dlgo/nn/layers.py
+++ dlgo/nn/layers.py
@@ -1,19 +1,23 @@
 import numpy as np
 
+
 def sigmoid_double(x):
     return 1.0 / (1.0 + np.exp(-x))
+
 
 def sigmoid(z):
     return np.vectorize(sigmoid_double)(z)
 
-def sigmonid_prime_double(x):
+
+def sigmoid_prime_double(x):
     return sigmoid_double(x) * (1 - sigmoid_double(x))
 
+
 def sigmoid_prime(z):
-    return np.vectorize(sigmonid_prime_double)(z)
+    return np.vectorize(sigmoid_prime_double)(z)
 
 
-class Layer:
+class Layer(object):
     def __init__(self):
         self.params = []
 
